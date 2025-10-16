bedcf6cd018fb5df47ff72b84788f91e6e547ca8
Ramesh143-code <pr6660270@gmail.com>
2025-10-16 07:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,8 @@ import torch
 import keras
 from PIL import Image
 
-
-# Load Model
 # Load Model
 model = keras.models.load_model('model.keras')
-
-
 
 # Interface
 
@@ -29,7 +25,7 @@ with st.container():
 
 # Input and Output 
 with st.container():
-    image = st.file_uploader('**:orange[UPLOAD XRAY IMAGE]**', accept_multiple_files=False)
+    image = st.file_uploader('**:orange[UPLOAD XRAY IMAGE]**', accept_multiple_files=False, key='xray_upload')
     if image:
         input_col, output_col = st.columns(2, border=False)
         with input_col:
@@ -50,74 +46,13 @@ with st.container():
     github_col, linkedin_col, huggingface_col = st.columns(3, border=False)
 
     with github_col:
-        st.link_button('**GITHUB**', icon=':material/code:', url='https://github.com/Ramesh143-code/todo-luxe', use_container_width=True, help='View the Project Source Code on GitHub ▶️')
+        st.link_button('**GITHUB**', icon=':material/code:', url='https://github.com/Ramesh143-code/Pneumonia-Detector', use_container_width=True, help='View the Project Source Code on GitHub ▶️', key='github_btn')
 
     with linkedin_col:
-        st.link_button('**LINKEDIN**', icon=':material/group_add:', url='https://www.linkedin.com/in/p-ramesh-477482304', use_container_width=True, help='Connect With Me 😊')
+        st.link_button('**LINKEDIN**', icon=':material/group_add:', url='https://www.linkedin.com/in/p-ramesh-477482304', use_container_width=True, help='Connect With Me 😊', key='linkedin_btn')
 
     with huggingface_col:
-        st.link_button('**STREAMLIT PROFILE**', icon=':material/deployed_code:', url='https://share.streamlit.io/user/ramesh143-code', use_container_width=True, help='MY DEPLOYED STREAMLIT PROJECTS 🤗')
-
-
-# setup
-import streamlit as st
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import torch 
-import keras
-from PIL import Image
-
-
-# Load Model
-# Load Model
-model = keras.models.load_model('model.keras')
-
-
-
-# Interface
-
-# title
-with st.container():
-    image_col, heading_col = st.columns([.5, 1.5], vertical_alignment='center')
-
-    with image_col:
-        st.image('Images/logo.jpg', width=150)
-
-    with heading_col:
-        st.write('# :rainbow[Pneumonia Detector]')
-        st.write('**:blue[Pneumonia Detector is a deep learning image classification model, that classifies the lungs x-ray images into Pneumonial and Normal.]**')
-
-# Input and Output 
-with st.container():
-    image = st.file_uploader('**:orange[UPLOAD XRAY IMAGE]**', accept_multiple_files=False)
-    if image:
-        input_col, output_col = st.columns(2, border=False)
-        with input_col:
-            st.image(image=image, use_container_width=True)
-
-        with output_col:
-            image = Image.open(image)
-            image = image.resize((180, 180))
-            image = tf.expand_dims(image, axis=0)
-            prediction = model.predict(image)
-            if prediction < 0.5:
-                st.image('Images/Normal.jpg')
-            else:
-                st.image('Images/Pneumonia.jpg')
-
-# Share Contents
-with st.container():
-    github_col, linkedin_col, huggingface_col = st.columns(3, border=False)
-
-    with github_col:
-        st.link_button('**GITHUB**', icon=':material/code:', url='https://github.com/Ramesh143-code/Pneumonia-Detector', use_container_width=True, help='View the Project Source Code on GitHub ▶️')
-
-    with linkedin_col:
-        st.link_button('**LINKEDIN**', icon=':material/group_add:', url='https://www.linkedin.com/in/p-ramesh-477482304', use_container_width=True, help='Connect With Me 😊')
-
-    with huggingface_col:
-        st.link_button('**STREAMLIT PROFILE**', icon=':material/deployed_code:', url='https://share.streamlit.io/user/ramesh143-code', use_container_width=True, help='MY DEPLOYED STREAMLIT PROJECTS 🤗')
+        st.link_button('**STREAMLIT PROFILE**', icon=':material/deployed_code:', url='https://share.streamlit.io/user/ramesh143-code', use_container_width=True, help='MY DEPLOYED STREAMLIT PROJECTS 🤗', key='streamlit_btn')
 
 
 # streamlit run app.py
